0497a09e952b73e38601829033ae6e390b2467e7
Manuel.Langer <Manuel.Langer@21851-21898>
2019-11-04 14:02:29 +0100

    Verbesserung Abfragen Gewicht Name etc

--- src/gra/man/de/PatienTest.java
+++ src/gra/man/de/PatienTest.java
@@ -2,6 +2,8 @@ package gra.man.de;
 
 import java.util.Scanner;
 
+import javax.print.DocFlavor.INPUT_STREAM;
+
 public class PatienTest {
 
 	public static void main(String[] args) {
@@ -11,57 +13,90 @@ public class PatienTest {
 		double groess =0;
 		String eing;
 		
-		boolean eingabe=false;
+		boolean neuerLoop=true;
 		
-		Scanner scan = new Scanner(System.in);
+		
 
-		
-		
-		while(eingabe == false) {
-			boolean eingGW =false;
-			boolean eingGR =false;
-			System.out.println("Bitte den Namen des Patienten eingeben: ");
-			n=	scan.nextLine();
-			System.out.println("Bitte geschlecht eingeben[männlich/weiblich]: ");
-			eing = scan.next();
+		while(neuerLoop==true) {
+				Scanner scan = new Scanner(System.in);
+				System.out.println("Bitte den Namen des Patienten eingeben: ");
+				n=scan.nextLine();
+				
+				boolean geschPrüfen = false;
+				while(geschPrüfen == false) {
+					System.out.println("Bitte geschlecht eingeben[männlich/weiblich]: ");
+					eing = scan.next();
+					
+					if(eing.equalsIgnoreCase("männlich") || eing.equalsIgnoreCase("m")) {
+						gesch = 'm';
+						geschPrüfen=true;
+					}
+					else if(eing.equalsIgnoreCase("weiblich") || eing.equalsIgnoreCase("w")) {
+						gesch = 'w';
+						geschPrüfen=true;
+					}else {
+						System.out.println("[!]Fehler Falsche Eingabe");
+					}
+				}
+				
+				boolean grPruefen =false;
+				while(grPruefen ==false) {
+										
+					try {
+						System.out.println("Bitte die größe des Patienten eingeben: ");
+						groess = scan.nextDouble();
+						grPruefen=true;
+						
+					}catch (Exception e) {
+						System.out.println("[!]Fehler Bitte nur Zahlenwerte eingeben (für Komma-Zahlen , verwenden...)");		
+						
+						scan.next();
+					}
+					
+				}
+
+				boolean gewPruefen =false;
+				while(gewPruefen==false) {
+					try {
+						System.out.println("Bitte das Gewicht des Patienten eingeben: ");
+						gew = scan.nextDouble();	
+						gewPruefen=true;
+					}catch (Exception e) {
+						System.out.println("[!]Fehler Bitte nur Zahlenwerte eingeben (für Komma-Zahlen , verwenden...)");		
+						scan.next();
+					}
+				
+				}
+				
+
+				
 			
-			if(eing.equalsIgnoreCase("männlich") || eing.equalsIgnoreCase("m")) {
-				gesch = 'm';
-			}
-			else if(eing.equalsIgnoreCase("weiblich") || eing.equalsIgnoreCase("w")) {
-				gesch = 'w';
-			}else {
-				System.out.println("[!]Fehler Falsche Eingabe");
-			}
+				
 			
-			try {
-				System.out.println("Bitte die größe des Patienten eingeben: ");
-				groess = scan.nextDouble();
-				eingGR = true;
-			}catch (Exception e) {
-				System.out.println("[!]Fehler Bitte nur Zahlenwerte eingeben (für Komma-Zahlen , verwenden...)");		
-				eingGR =false;
-			}
+			Patient max = new Patient(n, gesch, gew, groess);
+			max.getAlleWerte();
 			
-			try {
-				System.out.println("Bitte das Gewicht des Patienten eingeben: ");
-				gew = scan.nextDouble();
-				eingGW = true;
-			}catch (Exception e) {
-				System.out.println("[!]Fehler Bitte nur Zahlenwerte eingeben (für Komma-Zahlen , verwenden...)");		
-				eingGW = false;
-			}
-			
-			if(eingGR==true &&eingGW==true) {
-				eingabe=true;
+			boolean neue=true;
+			while(neue==true) {
+				System.out.println("\nWollen sie eine weitere Abfrage durchführen?");
+				String antwort =scan.next();
+				if(antwort.equalsIgnoreCase("y")) {
+					neue =false;
+					
+				}
+				if(antwort.equalsIgnoreCase("n")) {
+					neuerLoop=false;
+					neue =false;
+				}
 			}
 			
 		}
 		
+
+		
 	
 		
-		Patient max = new Patient(n, gesch, gew, groess);
-		max.getAlleWerte();
+		
 	}
 
 }
