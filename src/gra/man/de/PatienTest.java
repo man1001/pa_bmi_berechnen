5ce057915d3925310225f8c7d90a10c45923cf0d
Manuel.Langer <Manuel.Langer@21851-21898>
2019-11-04 10:51:16 +0100

    Interaktive eingabe

--- src/gra/man/de/PatienTest.java
+++ src/gra/man/de/PatienTest.java
@@ -1,9 +1,66 @@
 package gra.man.de;
 
+import java.util.Scanner;
+
 public class PatienTest {
 
 	public static void main(String[] args) {
-		Patient max = new Patient("Max Musterkerl", 'm', 75.0, 1.82);
+		String n ="";
+		char gesch = 'd';
+		double gew=0;
+		double groess =0;
+		String eing;
+		
+		boolean eingabe=false;
+		
+		Scanner scan = new Scanner(System.in);
+
+		
+		
+		while(eingabe == false) {
+			boolean eingGW =false;
+			boolean eingGR =false;
+			System.out.println("Bitte den Namen des Patienten eingeben: ");
+			n=	scan.nextLine();
+			System.out.println("Bitte geschlecht eingeben[männlich/weiblich]: ");
+			eing = scan.next();
+			
+			if(eing.equalsIgnoreCase("männlich") || eing.equalsIgnoreCase("m")) {
+				gesch = 'm';
+			}
+			else if(eing.equalsIgnoreCase("weiblich") || eing.equalsIgnoreCase("w")) {
+				gesch = 'w';
+			}else {
+				System.out.println("[!]Fehler Falsche Eingabe");
+			}
+			
+			try {
+				System.out.println("Bitte die größe des Patienten eingeben: ");
+				groess = scan.nextDouble();
+				eingGR = true;
+			}catch (Exception e) {
+				System.out.println("[!]Fehler Bitte nur Zahlenwerte eingeben (für Komma-Zahlen , verwenden...)");		
+				eingGR =false;
+			}
+			
+			try {
+				System.out.println("Bitte das Gewicht des Patienten eingeben: ");
+				gew = scan.nextDouble();
+				eingGW = true;
+			}catch (Exception e) {
+				System.out.println("[!]Fehler Bitte nur Zahlenwerte eingeben (für Komma-Zahlen , verwenden...)");		
+				eingGW = false;
+			}
+			
+			if(eingGR==true &&eingGW==true) {
+				eingabe=true;
+			}
+			
+		}
+		
+	
+		
+		Patient max = new Patient(n, gesch, gew, groess);
 		max.getAlleWerte();
 	}
 
